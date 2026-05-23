e957fdbae991b81716c62bf736549ed23792633b
John <johnwarmour@gmail.com>
2026-05-22 20:09:03 -0400

    Bind servers to 127.0.0.1 to restrict access to localhost only

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo "Make sure to map both 8000 and 8501 ports in your Docker run/create comman
 
 # Start FastAPI server on 8000:
 cd /fastAPI
-uvicorn server:app --host 0.0.0.0 --port 8000 &
+uvicorn server:app --host 127.0.0.1 --port 8000 &
 
 # Start Streamlit app on 8501:
 cd /streamlit
-streamlit run app.py --server.port 8501
+streamlit run app.py --server.port 8501 --server.address 127.0.0.1
